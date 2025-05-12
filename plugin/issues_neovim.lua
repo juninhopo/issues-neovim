780a4db1d5793d6e41dd27344781e7d71001a560
juninhopo <kelgaxd@gmail.com>
2025-05-11 23:24:07 -0400

    feat: remove autoreload

--- plugin/issues_neovim.lua
+++ plugin/issues_neovim.lua
@@ -1,6 +1,6 @@
 -- Check if the plugin has already been loaded
 if vim.g.loaded_issues_neovim then
-  return
+	return
 end
 vim.g.loaded_issues_neovim = true
 
@@ -9,33 +9,34 @@ local cmd = vim.api.nvim_create_user_command
 
 -- Command to open the main GitHub Issues interface
 cmd("IssuesNeovim", function()
-  require("issues_neovim").ui.open()
+	require("issues_neovim").ui.open()
 end, {
-  desc = "Open GitHub Issues interface",
+	desc = "Open GitHub Issues interface",
 })
 
 -- Command to run GitHub API diagnostics
 cmd("IssuesNeovimDiagnose", function()
-  require("issues_neovim").diagnose_github_api()
+	require("issues_neovim").diagnose_github_api()
 end, {
-  desc = "Diagnose GitHub API connection issues",
+	desc = "Diagnose GitHub API connection issues",
 })
 
 -- Command to configure the GitHub token
 cmd("IssuesNeovimSetupToken", function()
-  local token_path = vim.fn.expand("~/.config/github_token")
-  vim.cmd("edit " .. token_path)
+	local token_path = vim.fn.expand("~/.config/github_token")
+	vim.cmd("edit " .. token_path)
 end, {
-  desc = "Configure GitHub token",
+	desc = "Configure GitHub token",
 })
 
 -- Autocmd to initialize the plugin on Neovim startup
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    -- Load with delay to avoid blocking Neovim initialization
-    vim.defer_fn(function()
-      require("issues_neovim").setup()
-    end, 100)
-  end,
-  group = vim.api.nvim_create_augroup("IssuesNeovimSetup", { clear = true }),
-}) 
+--vim.api.nvim_create_autocmd("VimEnter", {
+--  callback = function()
+--    -- Load with delay to avoid blocking Neovim initialization
+--    vim.defer_fn(function()
+--     require("issues_neovim").setup()
+--   end, 100)
+--  end,
+--  group = vim.api.nvim_create_augroup("IssuesNeovimSetup", { clear = true }),
+-- })
+
